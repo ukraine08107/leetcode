d886783295cce5b82ebc16a89ca5177b132285e3
Andrii Sobianin <sobianin2008@gmail.com>
2022-07-18 23:57:39 -0500

    add 1 more solution

--- src/medium/LongestSubstringWithoutRepeatingCharacters.java
+++ src/medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -7,13 +7,14 @@ import java.util.TreeSet;
 
 public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
-     //   System.out.println(lengthOfLongestSubstring1("abcabcbb"));
-      //  System.out.println(lengthOfLongestSubstring1("bbbbb"));
-      //  System.out.println(lengthOfLongestSubstring1("pwwkew"));
-      //  System.out.println(lengthOfLongestSubstring1(" "));
-     //   System.out.println(lengthOfLongestSubstring1("cdd"));
-       // System.out.println(lengthOfLongestSubstring1("qrsvbspk"));
+        System.out.println(lengthOfLongestSubstring1("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring1("bbbbb"));
+        System.out.println(lengthOfLongestSubstring1("pwwkew"));
+        System.out.println(lengthOfLongestSubstring1(" "));
+        System.out.println(lengthOfLongestSubstring1("cdd"));
+        System.out.println(lengthOfLongestSubstring1("qrsvbspk"));
         System.out.println(lengthOfLongestSubstring1("abba"));
+                System.out.println(lengthOfLongestSubstring1("aab"));
     }
 
 
@@ -39,22 +40,43 @@ public class LongestSubstringWithoutRepeatingCharacters {
 
 
 
-    public static int lengthOfLongestSubstring1(String s) {
-    HashMap<Character,Integer> map  = new HashMap<>();
-    int counter = 0;
-    int result = 0;
+    public static int lengthOfLongestSubstring1(String s){
+        int begin = 0;
+        int end = 0;
+        int result = 0;
+        HashSet<Character> set = new HashSet<>();
         for (int i = 0; i <s.length() ; i++) {
-            if (!map.containsKey(s.charAt(i))){
-                map.put(s.charAt(i),i);
-                counter++;
+            if (set.contains(s.charAt(i))){
+                result = Math.max(result,end-begin);
+                set.remove(s.charAt(begin));
+                begin++;
+                i--;
             }else {
-                result = Math.max(result,counter);
-                counter = i - map.get(s.charAt(i));
-               map.put(s.charAt(i),i);
+                set.add(s.charAt(i));
+                    end++;
             }
         }
-        result = Math.max(result,counter);
-        return result;
+ result = Math.max(result,end-begin);
+return result;
+
+
+//
+//    HashMap<Character,Integer> map  = new HashMap<>();
+//    int counter = 0;
+//    int result = 0;
+//        for (int i = 0; i <s.length() ; i++) {
+//            if (!map.containsKey(s.charAt(i))){
+//                map.put(s.charAt(i),i);
+//                counter++;
+//            }else {
+//                result = Math.max(result,counter);
+//                counter = i - map.get(s.charAt(i));
+//               map.put(s.charAt(i),i);
+//            }
+//        }
+//        result = Math.max(result,counter);
+//        return result;
+
     }
 
 
